e30c8f6cbd8792c02a9e3e0fa6b5942af2a16d12
ManimaranHerring <think.maran@gmail.com>
2025-10-08 12:57:37 +0530

    Create index.js

---
+++ server/src/agents/index.js
@@ -0,0 +1,13 @@
+import * as qna from './qna.js';
+import * as proposal from './proposalAssistant.js';
+import * as dataHarmonizer from './dataHarmonizer.js';
+import * as esg from './esgAssistant.js';
+import * as forecasting from './forecasting.js';
+
+export default {
+  qna,
+  proposal,
+  dataHarmonizer,
+  esg,
+  forecasting
+};
